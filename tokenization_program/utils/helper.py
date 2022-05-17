b5313b31511d93e06ea8184628aca08ff38ab1a6
andMS <caro_ms00@hotmail.com>
2022-05-17 17:28:04 -0500

    Adding changes in style css sheet

--- tokenization_program/utils/helper.py
+++ tokenization_program/utils/helper.py
@@ -13,14 +13,10 @@ def get_token_list(tokens):
 
 
 def get_urls(found_tokens):
-    print('starting search of urls')
     tokens_url = {}
     for key, value in found_tokens.items():
         tokens_url[key] = []
         for values in value:
             url = f'/templates/{values[0].replace(".html", "")}/'
             tokens_url[key].append([url, values[0], values[1]])
-    print('finishing url search')
     return tokens_url
-
-
