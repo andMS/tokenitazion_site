e779aeee3ff60b3dc3e598f5e19c7fa6adb51880
andMS <caro_ms00@hotmail.com>
2022-05-17 17:57:10 -0500

    Experimentation with css

--- tokenization_site/settings.py
+++ tokenization_site/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'tokens_app/static')
+print(BASE_DIR)
+print(STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
